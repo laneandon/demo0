56a9593336f58817772235b01e036db75317c63a
lane <andontest102@gmail.com>
2016-02-26 17:38:03 +0800

    观察延迟补偿的例子

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -13,11 +13,12 @@ Template.postSubmit.events({
                 return alert(error.reason);
 
             // 显示结果，跳转页面
-            if (result.postExists)
+            if (result.postExists){
                 alert('This link has already been posted（该链接已经存在）');
-
-            //Router.go('postPage', {_id: result._id});
-            Router.go('postsList');
+            }
+            Router.go('postPage', {_id: result._id});
         });//*/
+
+        Router.go('postsList');
     }
 });
