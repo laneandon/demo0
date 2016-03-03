dda36dce4c378532e6bc613a00bfb183762bc3de
lane <andontest102@gmail.com>
2016-03-03 23:49:00 +0800

    评论调试完成

--- server/publications.js
+++ server/publications.js
@@ -1,14 +1,14 @@
-Meteor.publish("posts",function(){
+Meteor.publish("posts", function () {
     return Posts.find();
 })
 
-Meteor.publish('comments', function() {
-    return Comments.find();
+Meteor.publish('comments', function (postId) {
+    check(postId, String);
+    return Comments.find({postId: postId});
 });
 
 
-
 /*Meteor.publish('post',function(author){
-    //只是发布db中flagged的值为true的数据
-    return Posts.find({flagged: true, author: author});
-})*/
+ //只是发布db中flagged的值为true的数据
+ return Posts.find({flagged: true, author: author});
+ })*/
