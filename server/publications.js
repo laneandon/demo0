a16efb2966f582427b893ba3d8a2713b575ef9eb
lane <andontest102@gmail.com>
2016-03-03 17:59:34 +0800

    Commit初始完成

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,11 @@ Meteor.publish("posts",function(){
     return Posts.find();
 })
 
+Meteor.publish('comments', function() {
+    return Comments.find();
+});
+
+
 
 /*Meteor.publish('post',function(author){
     //只是发布db中flagged的值为true的数据
