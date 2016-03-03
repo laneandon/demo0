b6d6d3eddc4a8ae22faefcb2a78a3bfbe59f17f1
lane <andontest102@gmail.com>
2016-03-03 11:40:18 +0800

    错误验证已验证完成

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,28 +1,44 @@
 Template.postSubmit.events({
-    'submit form':function(e){
+    'submit form': function (e) {
         e.preventDefault();
 
-        var post={
-            url:$(e.target).find('[name=url]').val(),
-            title:$(e.target).find('[name=title]').val()
+        var post = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val()
         };
 
-        Meteor.call('postInsert', post, function(error, result) {
+        var errors = validatePost(post);
+        if (errors.title || errors.url) {
+            return Session.set('postSubmitErrors', errors);
+        }
+
+        Meteor.call('postInsert', post, function (error, result) {
             // 显示错误信息并退出
             if (error) {
                 throwError(error.reason);
             }
             // 显示结果，跳转页面
-            if (result.postExists){
+            if (result.postExists) {
                 throwError('This link has already been posted（该链接已经存在）');
             }
-            else
-            {
+            else {
                 Router.go('postsList');
             }
             //Router.go('postPage', {_id: result._id});//
         });//*/
         //可以用于观察Client和Server的两次运行posts.js文件的Meteor的内置方法
         //Router.go('postsList');
+    }
+});
+
+Template.postSubmit.onCreated(function () {
+    Session.set('postSubmitErrors', {});
+});
+Template.postSubmit.helpers({
+    errorMessage: function (field) {
+        return Session.get('postSubmitErrors')[field];
+    },
+    errorClass: function (field) {
+        return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
     }
 });
