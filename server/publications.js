61152434e9824865479ce54dfb4b1ee082abbbae
laneandon <andontest102@gmail.com>
2016-03-05 01:49:41 +0800

    高级发布机制

--- server/publications.js
+++ server/publications.js
@@ -8,6 +8,11 @@ Meteor.publish('comments', function (postId) {
 });
 
 
+Meteor.publish('notifications', function () {
+    //return Notifications.find();
+    return Notifications.find({userId: this.userId, read: false});
+});
+
 /*Meteor.publish('post',function(author){
  //只是发布db中flagged的值为true的数据
  return Posts.find({flagged: true, author: author});
